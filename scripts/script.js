0cf9c7515996ce439a90dfd61d006b46f1c569c3
SanderRepo <66312618+SanderRepo@users.noreply.github.com>
2023-11-02 17:00:53 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,47 @@
-// JavaScript Document
-console.log("Howdy!");
+// const articles = document.querySelectorAll("article");
+// const images = document.getElementsByClassName("hand");
+// let currentIndex = 0;
+
+// // Verberg alle artikelen behalve het eerste
+// for (let i = 1; i < articles.length; i++) {
+//   articles[i].style.display = "none";
+// }
+
+// images[images.length - 1].addEventListener("click", () => {
+//   articles.forEach((article) => {
+//     article.style.display = "none";
+//   });
+
+//   // Toon het volgende artikel en zorg ervoor dat de index wordt gecirkeld
+//   currentIndex = (currentIndex + 1) % articles.length;
+//   articles[currentIndex].style.display = "block";
+// });
+
+const articles = document.querySelectorAll("article");
+const images = document.getElementsByClassName("hand");
+const articleImage = document.getElementsByClassName("article-image");
+let currentIndex = 0;
+
+// Array van afbeeldingsbronnen
+const imageSources = [
+  "../images/gezichtsuitdrukkingen/gezichtsuitdrukking-1.png",
+  "../images/gezichtsuitdrukkingen/gezichtsuitdrukking-2.png",
+  "image3.jpg",
+  // Voeg hier de URL's toe voor de overige afbeeldingen
+];
+
+for (let i = 1; i < articles.length; i++) {
+  articles[i].style.display = "none";
+}
+
+images[images.length - 1].addEventListener("click", () => {
+  articles.forEach((article) => {
+    article.style.display = "none";
+  });
+
+  currentIndex = (currentIndex + 1) % articles.length;
+  articles[currentIndex].style.display = "block";
+
+  // Wijzig de afbeelding buiten de article
+  articleImage.src = imageSources[currentIndex];
+});
