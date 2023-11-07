991cd870ef48e637b3725fc9d4a82e97e7a39e94
SanderRepo <66312618+SanderRepo@users.noreply.github.com>
2023-11-07 16:23:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,27 +4,27 @@ const articleImage = document.getElementById("article-image");
 let currentIndex = 0;
 
 const imageSources = [
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-1.png",
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-2.png",
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-3.png",
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-4.png",
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-5.png",
-  "images/gezichtsuitdrukkingen/gezichtsuitdrukking-6.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-1.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-2.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-3.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-4.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-5.png",
+   "images/gezichtsuitdrukkingen/gezichtsuitdrukking-6.png",
 ];
 
 for (let i = 1; i < articles.length; i++) {
-  articles[i].style.display = "none";
+   articles[i].style.display = "none";
 }
 
 images[images.length - 1].addEventListener("click", () => {
-  articles.forEach((article) => {
-    article.style.display = "none";
-  });
+   articles.forEach((article) => {
+      article.style.display = "none";
+   });
 
-  currentIndex = (currentIndex + 1) % articles.length;
-  articles[currentIndex].style.display = "block";
+   currentIndex = (currentIndex + 1) % articles.length;
+   articles[currentIndex].style.display = "block";
 
-  articleImage.src = imageSources[currentIndex];
+   articleImage.src = imageSources[currentIndex];
 });
 
 // De selector van de images is gemaakt met behulp van ChatGPT. Een gedeelte van de code begrijp ik zelf niet.
@@ -37,17 +37,33 @@ const newSrc = "images/hand/machine-active.svg";
 const changeDuration = 300;
 
 const selectHook = document.getElementById("hook");
+var buttonSound = document.getElementById("button-sound");
 
 image.addEventListener("click", function () {
-  image.src = newSrc;
+   image.src = newSrc;
 
-  selectHook.classList.add("hook-animation");
+   selectHook.classList.add("hook-animation");
 
-  setTimeout(function () {
-    selectHook.classList.remove("hook-animation");
-  }, 5000);
+   setTimeout(function () {
+      selectHook.classList.remove("hook-animation");
+   }, 3000);
 
-  setTimeout(function () {
-    image.src = originalSrc;
-  }, changeDuration);
+   setTimeout(function () {
+      image.src = originalSrc;
+   }, changeDuration);
+
+   buttonSound.play();
+});
+
+// ----------------- //
+
+var easterEggSound = document.getElementById("easter-egg-sound");
+
+selectHook.addEventListener("click", function () {
+   if (!easterEggSound.paused) {
+      easterEggSound.pause();
+      easterEggSound.currentTime = 0;
+   }
+
+   easterEggSound.play();
 });
